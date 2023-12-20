1a79d347c4e6e52a4a162252b5c07d1fe5486fe3
Askar <a.khaimuldin@pin-up.tech>
2023-12-20 11:51:53 +0600

    added classes shape and point

---
+++ src/models/Point.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Point {
+    private double x;
+    private double y;
+
+    // constructor having both parameters
+    public Point(double x, double y) {
+        // do smth with that parameters
+    }
+
+    // getDistance(Point dest) - calculate the distance from current point to dest point
+    public double getDistance(Point dest) {
+        return 0;
+    }
+
+    // toString()
+
+}
---
+++ src/models/Shape.java
@@ -0,0 +1,32 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Shape {
+    // container of points (e.g. ArrayList<Point>)
+    private ArrayList<Point> points;
+
+    public Shape() {
+        points = new ArrayList<>();
+    }
+
+    // addPoint(Point) - adds a point into the container
+    public void addPoint(Point point) {
+        // add a point into the container
+    }
+
+    // calculatePerimeter()
+    public double calculatePerimeter() {
+        return 0;
+    }
+
+    // getLongestSide()
+    public double getLongestSide() {
+        return 0;
+    }
+
+    // getAverageSide()
+    public double getAverageSide() {
+        return 0;
+    }
+}
